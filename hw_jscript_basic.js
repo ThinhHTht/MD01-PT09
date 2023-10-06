8d06c48c231269769773a2bfd2fc74a42f149b14
ThinhHTht <145584562+ThinhHTht@users.noreply.github.com>
2023-10-06 19:06:05 +0700

    Add files via upload
    Js_basic

---
+++ hw_jscript_basic.js
@@ -0,0 +1,22 @@
+// Bài 3: Viết chương trình tính diện tích hình tròn.
+
+// let bankinhR = Number(prompt("Hay nhap ban kinh:"));
+// console.log (typeof bankinhR);
+// document.write(bankinhR*bankinhR*3.14)
+
+// Bai 4: Viết chương trình chu vi hình tròn.
+// let duongKinh = Number(prompt("Hay nhap duong kinh"));
+// document.write(duongKinh*3.14);
+
+//Bài1 : Hiển thị tab console một câu chào “wellcome to Rikkei Academy”.
+// console.log("wellcome to Rikkei Academy");
+// prompt("Moi ban nhap so tuoi cua minh:");
+
+// Bài 3: Hiển thị trên màn hình trình duyệt câu : “Quyết tâm học lập trình javascript”.
+// document.write("Quyết tâm học lập trình javascript");
+
+// Bài 4: Hiển thị hộp thoại thông báo với nội dung : “Chào mừng bạn đến với học viện Rikkei Academy”.
+// alert("Chào mừng bạn đến với học viện Rikkei Academy");
+
+// Bài 5: Hiển thị một hộp thoại thông báo và yêu cầu người dùng phải xác nhận “yes” or “no” với nội dung: “bạn đã đủ 18 tuổi chưa!”.
+confirm("bạn đã đủ 18 tuổi chưa!");
